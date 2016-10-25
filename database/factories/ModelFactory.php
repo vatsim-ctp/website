7c1e68ca0ac56a55d3f4ad7765bb8ddb416d5b15
Anthony Lawrence <artisan@anthonylawrence.me.uk>
2016-10-25 16:55:34 +0000

    Applied fixes from StyleCI
    [ci skip] [skip ci]

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -35,33 +35,33 @@ $factory->state(CTP\Models\Airfield::class, 'approved', function (Faker\Generato
 $factory->define(CTP\Models\User::class, function (Faker\Generator $faker) {
     return [
         'id'         => $faker->numberBetween(810000, 1300000),
-        "name_first" => $faker->firstName,
-        "name_last"  => $faker->lastName,
-        "email"      => $faker->email,
+        'name_first' => $faker->firstName,
+        'name_last'  => $faker->lastName,
+        'email'      => $faker->email,
     ];
 });
 
-$factory->state(CTP\Models\User::class, "admin", function (Faker\Generator $faker) {
+$factory->state(CTP\Models\User::class, 'admin', function (Faker\Generator $faker) {
     return [
-        "admin" => 1,
+        'admin' => 1,
     ];
 });
 
 $factory->define(CTP\Models\Vote::class, function (Faker\Generator $faker) {
     return [
-        "airfield_id" => CTP\Models\Airfield::inRandomOrder()->first()->id,
-        "user_id"  => factory(CTP\Models\User::class)->create()->id,
+        'airfield_id' => CTP\Models\Airfield::inRandomOrder()->first()->id,
+        'user_id'  => factory(CTP\Models\User::class)->create()->id,
     ];
 });
 
-$factory->state(CTP\Models\Vote::class, "arrival", function (Faker\Generator $faker) {
+$factory->state(CTP\Models\Vote::class, 'arrival', function (Faker\Generator $faker) {
     return [
-        "airfield_id" => CTP\Models\Airfield::arrival()->inRandomOrder()->first()->id,
+        'airfield_id' => CTP\Models\Airfield::arrival()->inRandomOrder()->first()->id,
     ];
 });
 
-$factory->state(CTP\Models\Vote::class, "departure", function (Faker\Generator $faker) {
+$factory->state(CTP\Models\Vote::class, 'departure', function (Faker\Generator $faker) {
     return [
-        "airfield_id" => CTP\Models\Airfield::departure()->inRandomOrder()->first()->id,
+        'airfield_id' => CTP\Models\Airfield::departure()->inRandomOrder()->first()->id,
     ];
 });
